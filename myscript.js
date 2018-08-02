b961884efe7fa9c5e4757e0d193aa7b34e811fac
chuppii <42010431+chuppii@users.noreply.github.com>
2018-08-02 09:06:26 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,13 @@
+function onload(){
+  alert("its done lol");
+}
+
+// window.addEventListener("load",onload);
+
+function zoom(that_image){
+  that_image.style.width="60%";
+}
+
+function unzoom(that_image){
+  that_image.style.width="50%";
+}
